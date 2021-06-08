abfc26cc0ee1a0b75cee055c8981ff3e44e9005c
Arpit-Anand <lostbullet1107@gmail.com>
2021-06-08 15:49:19 +0530

    Poor update

---
+++ modified_cohen-sutherland.c
@@ -0,0 +1,153 @@
+#include<stdio.h>
+#include<GL/glut.h>
+#include<stdbool.h>
+#include<string.h>
+int n;
+double x0[10], y0[10], x1[10], y1[10];
+double xmin=50, ymin=50, xmax=100, ymax=100;
+double xvmin =200, yvmin=200, xvmax=300, yvmax=300;
+const int TOP = 8;
+const int BOTTOM = 4;
+const int RIGHT = 2;
+const int LEFT = 1;
+
+int ComputeOutCode(double x, double y);
+
+void renderstring(float x, float y, char *str){
+	glColor3f(0.5,0.5,0.5);
+	glRasterPos2f(x,y);
+	for(int i=0; i<strlen(str); i++){
+		glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, str[i]);
+	}
+}
+
+void CohenSutherlandLineClipAndDraw(double x0, double y0, double x1, double y1){
+	int outcode0, outcode1, outcodeOut;
+	bool accept = false, done = false;
+	outcode0 = ComputeOutCode(x0,y0);
+	outcode1 = ComputeOutCode(x1, y1);
+	do{
+		if((outcode0 | outcode1) == 0){
+			accept = true;
+			done = true;
+		}
+		else if(outcode0 & outcode1){
+			done = true;
+		}
+		else{
+			double x,y;
+			outcodeOut = outcode0?outcode0:outcode1;
+			float slope = (y1-y0)/(x1-x0);
+			if(outcodeOut & TOP){
+				x = x0 + (1/slope)*(ymax - y0);
+				y = ymax;
+			}
+			else if(outcodeOut & BOTTOM){
+				x = x0 + (1/slope)*(ymin - y0);
+				y = ymin;
+			}
+			else if(outcodeOut & RIGHT){
+				x = xmax;
+				y = y0 + slope*(xmax - x0);
+			}
+			else{
+				x = xmin;
+				y = y0 + slope*(xmin - x0);
+			}
+			if(outcodeOut == outcode0){
+				x0 = x;
+				y0 = y;
+				outcode0 = ComputeOutCode(x0, y0);
+			}
+			else{
+				x1 = x;
+				y1 = y;
+				outcode1 = ComputeOutCode(x1,y1);
+			}
+		}
+	}while(!done);
+	
+	if(accept){
+		double sx = (xvmax-xvmin)/(xmax-xmin);
+		double sy = (yvmax - yvmin)/(ymax - ymin);
+		double vx0 = xvmin + (x0 - xmin)*sx;
+		double vy0 = yvmin + (y0 - ymin)*sy;
+		double vx1 = xvmin + (x1 - xmin)*sx;
+		double vy1 = yvmin + (y1 - ymin)*sy;
+		
+		glColor3f(1.0,0.0,0.0);
+		glBegin(GL_LINE_LOOP);
+		glVertex2f(xvmin, yvmin);
+		glVertex2f(xvmax, yvmin);
+		glVertex2f(xvmax, yvmax);
+		glVertex2f(xvmin, yvmax);
+		glEnd();
+		glColor3f(0.0,0.0,1.0);
+		glBegin(GL_LINES);
+		glVertex2d(vx0, vy0);
+		glVertex2d(vx1, vy1);
+		glEnd();
+	}
+}
+
+int ComputeOutCode(double x, double y){
+	int code = 0;
+	if(y > ymax)
+		code |= TOP;
+	else if(y < ymin)
+		code |= BOTTOM;
+	if(x > xmax)
+		code |= RIGHT;
+	else if(x < xmin)
+		code |= LEFT;
+	return code;
+}
+
+void display(){
+	glClear(GL_COLOR_BUFFER_BIT);
+	renderstring(2.0,480.0,"Arpit Anand 1BI18CS026");
+	glColor3f(1.0,0.0,0.0);
+	glBegin(GL_LINES);
+	for(int i=0; i<n; i++){
+		glVertex2d(x0[i], y0[i]);
+		glVertex2d(x1[i], y1[i]);
+	}
+	glEnd();
+	glColor3f(0.0,0.0,1.0);
+	glBegin(GL_LINE_LOOP);
+	glVertex2f(xmin, ymin);
+	glVertex2f(xmax, ymin);
+	glVertex2f(xmax, ymax);
+	glVertex2f(xmin, ymax);
+	glEnd();
+	for(int i=0; i<n; i++){
+		CohenSutherlandLineClipAndDraw(x0[i], y0[i], x1[i], y1[i]);
+	}
+	glFlush();
+}
+
+void myinit(){
+	glClearColor(1.0,1.0,1.0,1.0);
+	glColor3f(1.0,0.0,0.0);
+	glPointSize(1.0);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluOrtho2D(0.0, 499.0, 0.0, 499.0);
+}
+
+void main(int argc, char **argv){
+	printf("Enter the number of lines\n");
+	scanf("%d", &n);
+	printf("Enter end points(x1,y1,x2,y2)\n");
+	for(int i = 0; i<n; i++){
+		scanf("%lf%lf%lf%lf",&x0[i], &y0[i], &x1[i], &y1[i]);
+	}
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+	glutInitWindowSize(500,500);
+	glutInitWindowPosition(0,0);
+	glutCreateWindow("Line Clipping Algo");
+	glutDisplayFunc(display);
+	myinit();
+	glutMainLoop();
+}
